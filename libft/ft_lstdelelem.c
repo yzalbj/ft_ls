3500ed0571bade46c092a189b3b3fb054fd990c0
Joachim BLAZY <jblazy@e1r8p8.42.fr>
2017-12-21 19:41:56 +0100

    rectif

--- libft/ft_lstdelelem.c
+++ libft/ft_lstdelelem.c
@@ -6,7 +6,7 @@
 /*   By: jblazy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 19:56:08 by jblazy            #+#    #+#             */
-/*   Updated: 2017/11/16 18:57:09 by jblazy           ###   ########.fr       */
+/*   Updated: 2017/12/21 19:41:10 by jblazy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 
 void	ft_lstdelelem(void *content, size_t content_size)
 {
+	size_t	i;
+
+	i = 0;
 	if (!content)
 		return ;
-	free(content);
-	content_size = 0;
+	while (i < content_size)
+	{
+		free(content);
+		i++;
+	}
 }
